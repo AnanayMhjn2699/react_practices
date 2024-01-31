ff826d864fe854e81f02f4521ba21dac5bc3b6f1
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-01-31 11:46:43 +0530

    whack a mole game is done

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ each practice problem is in app.js only with the heading stated prefixed with "*
 3. pin matching as in we have 4 digit pin and buttons are there and selected digits appear in input box
 4. making a multi select dropdown which has toggle functionality and also shows no of states selected in the above button
 5. making a traffic light which changes colors from red->yellow->green->red->continues. it will show th red for 3 sec, then yellow for 2 sec and then green for 5 sec
+6. WhackAMole game
--- src/WhackAMole.js
+++ src/WhackAMole.js
@@ -2,18 +2,28 @@ import { useEffect, useState } from "react";
 
 const WhackAMole = () => {
   const [score, setScore] = useState(0);
+  const [count, setCount] = useState(0);
   const moleAppearInGridElem = Math.floor(Math.random() * 9 + 1);
 
   useEffect(() => {
     document.getElementById("ID" + moleAppearInGridElem).classList.add("mole");
 
-    // setTimeout(() => {
-    //   document
-    //     .getElementById("ID" + moleAppearInGridElem)
-    //     .classList.remove("mole");
-    // }, 1000);
-  }, [score]);
+    const intervalID = setTimeout(() => {
+      document
+        .getElementById("ID" + moleAppearInGridElem)
+        .classList.remove("mole");
+      setCount(count + 1);
+    }, 700);
 
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, [count]);
+
+  // /*tried creating handle click but the thing is it is not working properly because
+  //   the components are not getting mounted before this function for which tried using useEffect but
+  //   not working as useEffect cannot be inside the function and function cannot be written
+  //   inside useEffect thus had to give inline event handler */
   //   const handleClick = ({ id }) => {
   //     if (document.getElementById(id).classList.contains("mole"))
   //       setScore(score + 1);
@@ -27,33 +37,36 @@ const WhackAMole = () => {
           id="ID1"
           className="gridElem"
           onClick={() => {
-            if (document.getElementById("ID1").classList.contains("mole"))
+            if (document.getElementById("ID1").classList.contains("mole")) {
               setScore(score + 1);
-            document
-              .getElementById("ID" + moleAppearInGridElem)
-              .classList.remove("mole");
+              document
+                .getElementById("ID" + moleAppearInGridElem)
+                .classList.remove("mole");
+            }
           }}
         ></div>
         <div
           id="ID2"
           className="gridElem"
           onClick={() => {
-            if (document.getElementById("ID2").classList.contains("mole"))
+            if (document.getElementById("ID2").classList.contains("mole")) {
               setScore(score + 1);
-            document
-              .getElementById("ID" + moleAppearInGridElem)
-              .classList.remove("mole");
+              document
+                .getElementById("ID" + moleAppearInGridElem)
+                .classList.remove("mole");
+            }
           }}
         ></div>
         <div
           id="ID3"
           className="gridElem"
           onClick={() => {
-            if (document.getElementById("ID3").classList.contains("mole"))
+            if (document.getElementById("ID3").classList.contains("mole")) {
               setScore(score + 1);
-            document
-              .getElementById("ID" + moleAppearInGridElem)
-              .classList.remove("mole");
+              document
+                .getElementById("ID" + moleAppearInGridElem)
+                .classList.remove("mole");
+            }
           }}
         ></div>
       </div>
@@ -62,33 +75,36 @@ const WhackAMole = () => {
           id="ID4"
           className="gridElem"
           onClick={() => {
-            if (document.getElementById("ID4").classList.contains("mole"))
+            if (document.getElementById("ID4").classList.contains("mole")) {
               setScore(score + 1);
-            document
-              .getElementById("ID" + moleAppearInGridElem)
-              .classList.remove("mole");
+              document
+                .getElementById("ID" + moleAppearInGridElem)
+                .classList.remove("mole");
+            }
           }}
         ></div>
         <div
           id="ID5"
           className="gridElem"
           onClick={() => {
-            if (document.getElementById("ID5").classList.contains("mole"))
+            if (document.getElementById("ID5").classList.contains("mole")) {
               setScore(score + 1);
-            document
-              .getElementById("ID" + moleAppearInGridElem)
-              .classList.remove("mole");
+              document
+                .getElementById("ID" + moleAppearInGridElem)
+                .classList.remove("mole");
+            }
           }}
         ></div>
         <div
           id="ID6"
           className="gridElem"
           onClick={() => {
-            if (document.getElementById("ID6").classList.contains("mole"))
+            if (document.getElementById("ID6").classList.contains("mole")) {
               setScore(score + 1);
-            document
-              .getElementById("ID" + moleAppearInGridElem)
-              .classList.remove("mole");
+              document
+                .getElementById("ID" + moleAppearInGridElem)
+                .classList.remove("mole");
+            }
           }}
         ></div>
       </div>
@@ -97,33 +113,36 @@ const WhackAMole = () => {
           id="ID7"
           className="gridElem"
           onClick={() => {
-            if (document.getElementById("ID7").classList.contains("mole"))
+            if (document.getElementById("ID7").classList.contains("mole")) {
               setScore(score + 1);
-            document
-              .getElementById("ID" + moleAppearInGridElem)
-              .classList.remove("mole");
+              document
+                .getElementById("ID" + moleAppearInGridElem)
+                .classList.remove("mole");
+            }
           }}
         ></div>
         <div
           id="ID8"
           className="gridElem"
           onClick={() => {
-            if (document.getElementById("ID8").classList.contains("mole"))
+            if (document.getElementById("ID8").classList.contains("mole")) {
               setScore(score + 1);
-            document
-              .getElementById("ID" + moleAppearInGridElem)
-              .classList.remove("mole");
+              document
+                .getElementById("ID" + moleAppearInGridElem)
+                .classList.remove("mole");
+            }
           }}
         ></div>
         <div
           id="ID9"
           className="gridElem"
           onClick={() => {
-            if (document.getElementById("ID9").classList.contains("mole"))
+            if (document.getElementById("ID9").classList.contains("mole")) {
               setScore(score + 1);
-            document
-              .getElementById("ID" + moleAppearInGridElem)
-              .classList.remove("mole");
+              document
+                .getElementById("ID" + moleAppearInGridElem)
+                .classList.remove("mole");
+            }
           }}
         ></div>
       </div>
