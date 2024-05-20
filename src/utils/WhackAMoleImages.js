bd9ab4062bfc9e3aa38bac17902e66c066d26d49
Ananay Mahajan <ananaymahajan2699@gmail.com>
2024-05-20 15:24:30 +0530

    updated the code

--- src/utils/WhackAMoleImages.js
+++ src/utils/WhackAMoleImages.js
@@ -1,5 +1,7 @@
 export const EMPTY_IMAGE =
-  "https://web-dev-junkie-files.s3.amazonaws.com/hole.png";
+  //"https://web-dev-junkie-files.s3.amazonaws.com/hole.png";
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjxmDcqrn4tnQQIsZjsJds91d0JfIHVVN1bsb2EH_b5Q&s";
 
 export const MOLE_IMAGE =
-  "https://web-dev-junkie-files.s3.amazonaws.com/mole.png";
+  //"https://web-dev-junkie-files.s3.amazonaws.com/mole.png";
+  "https://e7.pngegg.com/pngimages/616/150/png-clipart-mole-cartoon-mole-day-s-mammal-food-thumbnail.png";
